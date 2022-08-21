2ee490c7222550400f23cb03fe5ec51b8210b333
Danielle Broadaway <danielle.broadaway@gmail.com>
2022-08-21 15:17:10 -0500

    Responsive Nav
    Added Media Queries for max-width 1024
    
    created quote objects and arrays for quote generator and design prompt generator.

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+const quotes = [
+    {
+        quote: "A car is a tool. Over time the efficiency and comfort of a tool becomes more important than however easy it was to learn.",
+        author: "Hans van de Bruggen",
+        source: "https://uxdesign.cc/i-made-a-prototype-elon-musk-didnt-like-it-then-the-internet-went-nuts-9cef407ec686"
+    },
+    {
+        quote: "It avoids the edge cases. You should, too. There is danger and misery at the edges.",
+        author: "Douglas Crockford",
+        source: "https://www.oreilly.com/library/view/javascript-the-good/9780596517748/"
+    },
+    {
+        quote: "If you judge a fish by it\'s ability to climb a tree, it will live it'\'s whole life believing it is stupid",
+        author: "Not Albert Einstein"
+    }
+       
+]
+
+const designPrompts = ["The voices in my head", "An obese pigeon on South Street", "Danger and misery at the edges", "A fish trying to climb a tree"]
